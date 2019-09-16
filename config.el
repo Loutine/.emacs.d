0bb55700289482b719c1ceff435cf85d11211d8c
Loutine <popipuo@qq.com>
2019-09-16 20:36:55 +0800

    a light version

--- config.el
+++ config.el
@@ -1,20 +1,11 @@
 (defun set-font (english chinese english-size chinese-size)
    (set-face-attribute 'default nil :font
 		       (format   "%s:pixelsize=%d"  english english-size))
-   (dolist (charset '(kana han symbol cjk-misc bopomofo))
+   (if (display-graphic-p) (dolist (charset '(kana han symbol cjk-misc bopomofo))
      (set-fontset-font (frame-parameter nil 'font) charset
-		       (font-spec :family chinese :size chinese-size))))
+		       (font-spec :family chinese :size chinese-size)))))
 
-(set-font   "Hack" "楷体" 35 41)
-
-(add-hook 'after-make-frame-functions
-	  (lambda (frame)
-	    (let ((mode (if (display-graphic-p frame) 'light 'dark)))
-	      (set-frame-parameter frame 'background-mode mode)
-	      (set-terminal-parameter frame 'background-mode mode))
-	    (enable-theme 'solarized)))
-(load-theme 'solarized t)
-(set-frame-parameter nil 'background-mode 'light)
+(set-font   "Hack Nerd Font Mono" "WenQuanYi Zen Hei Mono" 35 41)
 
 (add-hook 'org-mode-hook
 	  (lambda()
@@ -31,53 +22,14 @@
 
 (require 'ox-twbs)
 
-(require 'yasnippet)
-(yas/global-mode 1)
+(global-company-mode 1)
+(setq company-idle-delay 0)
+(setq company-minimum-prefix-length 1)
+(setq company-selection-wrap-around t)
+(company-tng-configure-default)
 
-(require 'auto-complete)
-(require 'auto-complete-config)
-(require 'auto-complete-clang)
-;; 设置不自动启动
-(setq ac-auto-start nil)  
-;; 设置响应时间 0.5
-(setq ac-quick-help-delay 0.1)  
-;;(ac-set-trigger-key "TAB-a")  
-;;(define-key ac-mode-map  [(control tab)] 'auto-complete)  
-;; 提示快捷键为 M-/
-(define-key ac-mode-map  (kbd "TAB") 'auto-complete) 
-(defun my-ac-config ()
-  (setq-default ac-sources '(ac-source-abbrev ac-source-dictionary ac-source-words-in-same-mode-buffers))
-  (add-hook 'emacs-lisp-mode-hook 'ac-emacs-lisp-mode-setup)
-  (add-hook 'c-mode-common-hook 'ac-cc-mode-setup)
-  (add-hook 'ruby-mode-hook 'ac-ruby-mode-setup)
-  (add-hook 'css-mode-hook 'ac-css-mode-setup)
-  (add-hook 'auto-complete-mode-hook 'ac-common-setup)
-  (global-auto-complete-mode t))  
-(defun my-ac-cc-mode-setup ()  
-  (setq ac-sources (append '(ac-source-clang ac-source-yasnippet) ac-sources)))  
-(add-hook 'c-mode-common-hook 'my-ac-cc-mode-setup)
-
-(setq ac-clang-flags
-      (mapcar (lambda (item)(concat "-I" item))
-	      (split-string
-	       "
- C:/msys64/mingw64/include/c++/9.2.0
- C:/msys64/mingw64/include/c++/9.2.0/x86_64-w64-mingw32
- C:/msys64/mingw64/include/c++/9.2.0/backward
- C:/msys64/mingw64/lib/gcc/x86_64-w64-mingw32/9.2.0/include
- C:/msys64/mingw64/include
- C:/msys64/mingw64/lib/gcc/x86_64-w64-mingw32/9.2.0/include-fixed
- C:/msys64/mingw64/x86_64-w64-mingw32/include
-"
-	       )))
-
-;; ac-source-gtags  
-(my-ac-config)  
-(ac-config-default)
-;; 结束
-
-(add-hook 'python-mode-hook 'jedi:setup)
-(setq jedi:complete-on-dot t)
+(add-hook 'rust-mode-hook #'racer-mode)
+(add-hook 'racer-mode-hook #'eldoc-mode)
 
 (let ((my-path "C:/Program Files/MiKTeX 2.9/miktex/bin/x64"))
      (setenv "PATH" (concat my-path ":" (getenv "PATH"))) 
@@ -85,7 +37,7 @@
 
 ;;AUCTex configuration
 ;;set the path of latex
-(require 'tex-site)
+;;(require 'tex-site)
 ;;set preview mode
 (setq org-latex-create-formula-image-program 'dvipng)
 ;;set auto save
--- init.el
+++ init.el
@@ -23,7 +23,12 @@
    [default default default italic underline success warning error])
  '(ansi-color-names-vector
    ["black" "#d55e00" "#009e73" "#f8ec59" "#0072b2" "#cc79a7" "#56b4e9" "white"])
- '(custom-enabled-themes (quote (solarized)))
+ '(company-backends
+   (quote
+    (company-bbdb company-eclim company-semantic company-clang company-xcode company-cmake company-capf company-files
+		  (company-dabbrev-code company-gtags company-etags company-keywords)
+		  company-oddmuse company-dabbrev)))
+ '(custom-enabled-themes (quote (solarized-dark)))
  '(custom-safe-themes
    (quote
     ("1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d91ef4e714f05fff2070da7ca452980999f5361209e679ee988e3c432df24347" "0598c6a29e13e7112cfbc2f523e31927ab7dce56ebb2016b567e1eff6dc1fd4f" "8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" default)))
@@ -36,7 +41,7 @@
      ("melpa-stable" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa-stable/"))))
  '(package-selected-packages
    (quote
-    (htmlize jedi ox-twbs auctex auto-complete-clang yasnippet auto-complete color-theme-solarized ## helm org-bullets)))
+    (solarized-theme racer company htmlize ox-twbs auctex yasnippet color-theme-solarized ## helm org-bullets)))
  '(scroll-bar-mode nil)
  '(tool-bar-mode nil))
 (custom-set-faces
