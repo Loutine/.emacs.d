14e5c9d437b9043345301c92fcbf921da8859453
Loutine <popipuo@qq.com>
2019-11-17 15:19:45 +0800

    I'm neo

--- config.el
+++ config.el
@@ -6,32 +6,6 @@
     ("gnu" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
     ("melpa-stable" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa-stable/")))
 
-(defun require-package (package &optional min-version no-refresh)
-  "Install given PACKAGE, optionally requiring MIN-VERSION.
-If NO-REFRESH is non-nil, the available package lists will not be
-re-downloaded in order to locate PACKAGE."
-  (if (package-installed-p package min-version)
-    t
-    (if (or (assoc package package-archive-contents) no-refresh)
-      (if (boundp 'package-selected-packages)
-	;; Record this as a package the user installed explicitly
-	(package-install package nil)
-	(package-install package))
-      (progn
-	(package-refresh-contents)
-	(require-package package min-version t)))))
-(defun maybe-require-package (package &optional min-version no-refresh)
-  "Try to install PACKAGE, and return non-nil if successful.
-In the event of failure, return nil and print a warning message.
-Optionally require MIN-VERSION.  If NO-REFRESH is non-nil, the
-available package lists will not be re-downloaded in order to
-locate PACKAGE."
-  (condition-case err
-    (require-package package min-version no-refresh)
-    (error
-      (message "Couldn't install optional package `%s': %S" package err)
-      nil)))
-
 (setq
      backup-by-copying t ; 自动备份
      backup-directory-alist
