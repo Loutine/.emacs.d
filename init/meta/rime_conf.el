190140a26786dcf94ca36eb1efe3e0259f5644a6
Loutine <popipuo@qq.com>
2020-05-31 23:17:19 +0800

    add sdcv

---
+++ init/meta/rime_conf.el
@@ -0,0 +1,10 @@
+(use-package rime
+  :demand
+  :custom
+  (default-input-method "rime")
+  (rime-show-candidate 'popup)
+  (rime-user-data-dir "~/.config/fcitx/rime")
+  (rime-translate-keybindings
+   '("C-f" "C-b" "C-n" "C-p" "C-g" "C-`"))
+)
+(provide 'rime_conf)
