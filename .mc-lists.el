5daa1c0c386df49a062febb4ab7d286811f75670
Loutine <popipuo@qq.com>
2021-12-16 18:28:36 +0800

    12.16

--- .mc-lists.el
+++ .mc-lists.el
@@ -3,6 +3,7 @@
 
 (setq mc/cmds-to-run-for-all
       '(
+	markdown-outdent-or-delete
 	org-self-insert-command
 	paredit-forward-slurp-sexp
 	))
--- customized.el
+++ customized.el
@@ -7,7 +7,7 @@
  '(company-box-icons-alist 'company-box-icons-icons-in-terminal)
  '(create-lockfiles nil)
  '(custom-raised-buttons t)
- '(default-input-method "rime")
+ '(default-input-method "rime" nil nil "Customized with use-package rime")
  '(delete-old-versions t)
  '(eldoc-idle-delay 0)
  '(fringe-mode 20 nil (fringe))
@@ -27,10 +27,10 @@
    '(bing-dict ## yas-jit yasnippet-snippets geiser perspective unicode-fonts telega org-pomodoro exwm org-noter-pdftools company-quickhelp company zmq youdao-dictionary which-key web-server web-mode vterm-toggle use-package treemacs-projectile tree-mode tabbar-ruler sudo-edit rustic realgud rainbow-mode rainbow-delimiters racket-mode quickrun python-mode python-environment pyenv-mode-auto proof-general phi-search persist pdf-tools paredit ox-twbs org-roam-server org-ql org-present org-noter org-mind-map org-bullets multiple-cursors lsp-ui lsp-python-ms lsp-ivy levenshtein leetcode lcr kubernetes keyfreq julia-mode htmlize highlight-numbers highlight-indent-guides haskell-mode goto-chg flycheck exec-path-from-shell epresent epc elpy ein edit-indirect doom-themes doom-modeline dashboard dap-mode counsel-projectile company-web company-org-roam company-lsp company-coq company-box command-log-mode color-identifiers-mode cnfonts cmake-mode cask caml benchmark-init auctex adoc-mode))
  '(posframe-mouse-banish t)
  '(reb-re-syntax 'string)
- '(rime-show-candidate 'posframe)
  '(scroll-bar-mode nil)
  '(tool-bar-mode t)
  '(truncate-lines nil)
+ '(warning-suppress-log-types '((comp) (lsp-mode)))
  '(warning-suppress-types '((lsp-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
